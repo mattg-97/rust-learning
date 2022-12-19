2a4f95ab3410433d87533d0316af7099d5ebedcd
mattg-97 <mattjohngill@gmail.com>
2022-12-19 15:49:07 +0000

    Rust learning safety commit

---
+++ rectangles/target/debug/deps/rectangles-7ca5bc7ec4205777.d
@@ -0,0 +1,5 @@
+/Users/matthewgill/Documents/rust/rust-learning/rectangles/target/debug/deps/rectangles-7ca5bc7ec4205777: src/main.rs
+
+/Users/matthewgill/Documents/rust/rust-learning/rectangles/target/debug/deps/rectangles-7ca5bc7ec4205777.d: src/main.rs
+
+src/main.rs:
---
+++ rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+/Users/matthewgill/Documents/rust/rust-learning/rectangles/target/debug/rectangles: /Users/matthewgill/Documents/rust/rust-learning/rectangles/src/main.rs
